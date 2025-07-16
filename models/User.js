86be34693d41fe939143eed5c66482722026f15c
Rex Ryle Urtezuela <reksryleurtezuela@gmail.com>
2025-07-17 06:20:29 +0800

    Resolved merge conflicts and updated backend

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-<<<<<<< HEAD
 
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
@@ -21,15 +20,6 @@ const userSchema = new mongoose.Schema({
 
   idImagePath: String,
   submittedAt: { type: Date, default: Date.now },
-=======
-  firstName: String,
-  lastName: String,
-  age: { type: Number, required: true },
-  role: { type: String, enum: ['resident', 'responder', 'admin'], default: 'resident' },
-  status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
-  idImagePath: String,
-  submittedAt: Date,
->>>>>>> 7d04e962bd450f07664b236dfc8837d0e562fecb
   approvedAt: Date,
 });
 
